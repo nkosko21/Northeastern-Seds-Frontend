f3d410c8b3150e72bff94a7cb1812c1ec5bbcf8f
nkosko21 <nickoy7@gmail.com>
2024-08-06 17:07:30 -0400

    events

---
+++ src/Data/Outreach.tsx
@@ -0,0 +1,30 @@
+export const outreach = [
+  'Building bridges event in collaboration with the Northeastern Center for STEM Education. '
+  + 'The event was an informational exposition designed for younger students to learn about '
+  + 'STEM topics.',
+
+  'At last year’s event 103 students from 32 schools and 5 states attendedand 100%'
+  + 'of surveyed students would recommend the event to others interested inlearning more about STEM.',
+
+  'While at the building bridges event, SEDS showed off our different projects' 
+  + 'and helped answer questions about what it is like working on STEM projects,' 
+  + 'particularly in the area of space technology.',
+
+  'Showcased the Rover to 40+ local high school students from lower-income communities through an '
+  + 'event organized by the Center for STEM Education at Northeastern, '
+  + 'where some students got to drive the Rover around campus.',
+
+  'Represented Northeastern’s Institute for Experiential Robotics at the '
+  + 'MassRobotics Block Party and demonstrated the Rover to thousands of professionals in the robotics space.',
+
+  'Showcased the Mars rover at the kickoff event for Mass STEM Week, where we showcased '
+  + 'the rover to over 50 local high school students, as well as Former Massachusetts Governor '
+  + 'Charlie Baker. This was an extremely high profile event, where Governor Baker and '
+  + 'President Aoun discussed the importance of STEM education and experiential learning.',
+
+  'Hosted an exhibit at the Museum of Science (MOS) for Computer Science Education Weekend, '
+  + 'where we demonstrated our rover to thousands of grade-school students and their '
+  + 'families in hopes of inspiring them to become future engineers and scientists.',
+
+  'Attended an event hosted by Roxbury Robotics where we showcased our Rover to underprivileged students.'
+];
--- src/Pages/Events.tsx
+++ src/Pages/Events.tsx
@@ -1,12 +1,32 @@
 import React from "react";
 import Navigationbar from "../Components/Navigationbar";
+import './Events.css';
+import { outreach } from "../Data/Outreach";
 
 export default function Events() {
 
   return(
   <>
     <Navigationbar highlighted={'events'}/>
-    events
+    <div className="event-container">
+      <article>
+        {outreach.map(example => <OutreachExample text={example} />)}
+      </article>
+      <aside>
+        <h2 style={{ color: '#de8149' }}>Outreach</h2>
+        <p className="outreach-desc">
+          SEDS is committed to educating other Northeastern students on campus about professional 
+          and engineering skills and reaching the Boston area to promote the role 
+          Northeastern University has in STEM fields. The events described below cover the outreach we have done in the past few years:
+        </p>
+      </aside>
+    </div>
   </>
   );
 }
+
+const OutreachExample = ({text} : {text: string}) => (
+  <div className="outreachExample">
+    <p>{text}</p>
+  </div>
+);
